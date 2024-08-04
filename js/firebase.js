8cdfa7a9145f5ff349c102776a4d5cb1e1cf7244
muhammadh9 <m.huzaif999@gmail.com>
2024-08-04 10:53:37 +0500

    mart

---
+++ js/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+ const firebaseConfig = {
+  apiKey: "AIzaSyCmMiYLqj4SUVWckFhLBZPwnrbmxw-fCLQ",
+  authDomain: "smit-batch-11-project.firebaseapp.com",
+  projectId: "smit-batch-11-project",
+  storageBucket: "smit-batch-11-project.appspot.com",
+  messagingSenderId: "1091984870314",
+  appId: "1:1091984870314:web:00b669a7ea9e862996b9d3"
+};
+ 
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+
